f561aa490c122dacffb5f01550a65efbe7723a64
miguelag99 <miguelag99@hotmail.com>
2021-03-17 12:55:31 +0100

    Bbox 2D

--- Source/pruebas.py
+++ Source/pruebas.py
@@ -1,14 +1,60 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import os
 
-path = "/home/miguel/TFG/Stereo-Vision/"
 
-imgL = cv2.imread(path+"/Datasets/000000.png",-1)
-imgR = cv2.imread(path+"/Datasets/000000_R.png",-1)
-assert(imgL is not None)
-assert(imgR is not None)
+#def draw_2D_bbox(img,):
 
-#plt.imshow(cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB))
-#plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-#plt.show()
+
+def main():
+    path = []
+
+    path.append("/home/miguel/TFG/Stereo-Vision/Datasets/image_left/")
+    path.append("/home/miguel/TFG/Stereo-Vision/Datasets/label_2/")
+    path.append("/home/miguel/TFG/Stereo-Vision/Datasets/camera_param/calib/")
+
+    i = 250
+
+    image_file = sorted(os.listdir(path[0]))[i]
+    label_file = sorted(os.listdir(path[1]))[i]
+    param = sorted(os.listdir(path[2]))[i]
+
+    imgL = cv2.imread(path[0]+image_file,1)
+    imgL = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
+    assert(imgL is not None)
+
+    camera_param = open(path[2]+param,'r')
+    matrix = camera_param.readlines()
+    matrix = (matrix[2].split(':'))[1].split(' ')
+    print(matrix)
+    Labels = open(path[1]+label_file,'r')
+    camera_param = open()
+    for label in Labels.readlines():
+        field = label.split(' ')
+        if field[0] != 'DontCare':
+            point1 = (int(float(field[4])),int(float(field[5])))
+            point2 = (int(float(field[6])),int(float(field[7])))
+            center_world = (int(float(field[11])),int(float(field[12])),int(float(field[13])))
+            cv2.rectangle(imgL,point1,point2,(0,255,0),1)
+            #cv2.circle(imgL,center,100,(0,0,255))
+        
+    Labels.close()
+
+    #imgR = cv2.imread(path[1]+image,0)
+    #assert(imgR is not None)
+
+    #stereo = cv2.StereoBM_create(numDisparities=112, blockSize=39)
+    #disparity = stereo.compute(imgL,imgR)
+
+    #plt.imshow(disparity,'gray')
+    
+    plt.imshow(imgL)
+    plt.show()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
